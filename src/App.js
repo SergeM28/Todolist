46b5fa3ccf023b2809f78fa8bf29ed918e6fd807
serge.muradian@gmail.com <serge.muradian@gmail.com>
2023-01-31 20:36:04 +0300

    Create adding tasks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  const handleChange = (e) => {
+    setNewTask(e.target.value)
+  }
+
+  const addTask = () => {
+    setTodoList([...todoList, newTask])
+  }
+
   return (
     <div className="App">
+      <div className='addTask'>
+          <input onChange={handleChange}/>
+          <button onClick={addTask}>Add Task</button>
+      </div>
+      <div className='list'>
+        {todoList.map((task) => {
+          return <h1>{task}</h1>
+        })}
+      </div>
     </div>
   );
 }
